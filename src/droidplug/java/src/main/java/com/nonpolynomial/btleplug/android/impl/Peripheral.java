194bb1c4bbb3ca3674a628a5f360d6f4bbba4d6c
Gedgy Gedgy <gedgygedgy@protonmail.com>
2021-06-24 19:33:03 -0400

    Implement a proper command queue in Peripheral

--- src/droidplug/java/src/main/java/com/nonpolynomial/btleplug/android/impl/Peripheral.java
+++ src/droidplug/java/src/main/java/com/nonpolynomial/btleplug/android/impl/Peripheral.java
@@ -4,6 +4,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -17,8 +18,9 @@ class Peripheral {
     private final Callback callback;
     private boolean connected = false;
 
-    private final Queue<Future.Waker<Void>> connectQueue = new LinkedList<>();
-    private final Queue<Future.Waker<Void>> disconnectQueue = new LinkedList<>();
+    private final Queue<Runnable> commandQueue = new LinkedList<>();
+    private boolean executingCommand = false;
+    private CommandCallback commandCallback;
 
     public Peripheral(String address) {
         this.device = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(address);
@@ -26,90 +28,170 @@ class Peripheral {
     }
 
     public Future<Void> connect() {
-        return asyncWithWaker((waker) -> {
-            synchronized (this) {
-                if (this.gatt == null) {
-                    try {
-                        this.connectQueue.add(waker);
-                        this.gatt = this.device.connectGatt(null, false, this.callback);
-                    } catch (SecurityException ex) {
-                        throw new PermissionDeniedException(ex);
+        Future.Waker<Void> waker = Future.create();
+        synchronized (this) {
+            this.queueCommand(() -> {
+                this.asyncWithWaker(waker, () -> {
+                    CommandCallback callback = new CommandCallback() {
+                        @Override
+                        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+                            Peripheral.this.asyncWithWaker(waker, () -> {
+                                if (status == BluetoothGatt.GATT_SUCCESS) {
+                                    if (newState == BluetoothGatt.STATE_CONNECTED) {
+                                        Peripheral.this.wakeCommand(waker, null);
+                                    }
+                                } else {
+                                    throw new NotConnectedException();
+                                }
+                            });
+                        }
+                    };
+
+                    if (this.connected) {
+                        Peripheral.this.wakeCommand(waker, null);
+                    } else if (this.gatt == null) {
+                        try {
+                            this.setCommandCallback(callback);
+                            this.gatt = this.device.connectGatt(null, false, this.callback);
+                        } catch (SecurityException ex) {
+                            throw new PermissionDeniedException(ex);
+                        }
+                    } else {
+                        this.setCommandCallback(callback);
+                        if (!this.gatt.connect()) {
+                            throw new RuntimeException("Unable to reconnect to device");
+                        }
                     }
-                } else if (this.connected) {
-                    waker.wake(null);
-                } else if (this.gatt.connect()) {
-                    this.connectQueue.add(waker);
-                } else {
-                    throw new RuntimeException("Unable to reconnect to device");
-                }
-            }
-        });
+                });
+            });
+        }
+        return waker.getFuture();
     }
 
     public Future<Void> disconnect() {
-        return asyncWithWaker((waker) -> {
-            synchronized (this) {
-                if (this.gatt == null || !this.connected) {
-                    waker.wake(null);
-                } else {
-                    this.gatt.disconnect();
-                    this.disconnectQueue.add(waker);
-                }
-            }
-        });
+        Future.Waker<Void> waker = Future.create();
+        synchronized (this) {
+            this.queueCommand(() -> {
+                this.asyncWithWaker(waker, () -> {
+                    if (!this.connected) {
+                        Peripheral.this.wakeCommand(waker, null);
+                    } else {
+                        this.setCommandCallback(new CommandCallback() {
+                            @Override
+                            public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+                                Peripheral.this.asyncWithWaker(waker, () -> {
+                                    if (status == BluetoothGatt.GATT_SUCCESS) {
+                                        if (newState == BluetoothGatt.STATE_DISCONNECTED) {
+                                            Peripheral.this.wakeCommand(waker, null);
+                                        }
+                                    } else {
+                                        throw new RuntimeException("Unable to disconnect");
+                                    }
+                                });
+                            }
+                        });
+                        this.gatt.disconnect();
+                    }
+                });
+            });
+        }
+        return waker.getFuture();
     }
 
     public boolean isConnected() {
         return this.connected;
     }
 
+    private void queueCommand(Runnable callback) {
+        if (this.executingCommand) {
+            this.commandQueue.add(callback);
+        } else {
+            this.executingCommand = true;
+            callback.run();
+        }
+    }
+
+    private void setCommandCallback(CommandCallback callback) {
+        assert this.commandCallback == null;
+        this.commandCallback = callback;
+    }
+
+    private void runNextCommand() {
+        assert this.executingCommand;
+        this.commandCallback = null;
+        if (this.commandQueue.isEmpty()) {
+            this.executingCommand = false;
+        } else {
+            Runnable callback = this.commandQueue.remove();
+            callback.run();
+        }
+    }
+
+    private <T> void wakeCommand(Future.Waker<T> waker, T result) {
+        waker.wake(result);
+        this.runNextCommand();
+    }
+
+    private <T> void asyncWithWaker(Future.Waker<T> waker, Runnable callback) {
+        try {
+            callback.run();
+        } catch (Throwable ex) {
+            waker.wakeWithThrowable(ex);
+            this.runNextCommand();
+        }
+    }
+
     private class Callback extends BluetoothGattCallback {
         @Override
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
             synchronized (Peripheral.this) {
-                if (status == BluetoothGatt.GATT_SUCCESS) {
-                    switch (newState) {
-                        case BluetoothGatt.STATE_CONNECTED:
-                            Peripheral.this.connected = true;
-                            wakeQueue(Peripheral.this.connectQueue, null);
-                            break;
-                        case BluetoothGatt.STATE_DISCONNECTED:
-                            Peripheral.this.connected = false;
-                            wakeQueue(Peripheral.this.disconnectQueue, null);
-                            break;
-                    }
-                } else if (newState == BluetoothGatt.STATE_DISCONNECTED) {
-                    wakeQueueWithThrowable(Peripheral.this.connectQueue, new NotConnectedException());
+                switch (newState) {
+                    case BluetoothGatt.STATE_CONNECTED:
+                        Peripheral.this.connected = true;
+                        break;
+                    case BluetoothGatt.STATE_DISCONNECTED:
+                        Peripheral.this.connected = false;
+                        break;
+                }
+                if (Peripheral.this.commandCallback != null) {
+                    Peripheral.this.commandCallback.onConnectionStateChange(gatt, status, newState);
+                }
+            }
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            synchronized (Peripheral.this) {
+                if (Peripheral.this.commandCallback != null) {
+                    Peripheral.this.commandCallback.onCharacteristicRead(gatt, characteristic, status);
+                }
+            }
+        }
+
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            synchronized (Peripheral.this) {
+                if (Peripheral.this.commandCallback != null) {
+                    Peripheral.this.commandCallback.onCharacteristicWrite(gatt, characteristic, status);
                 }
             }
         }
     }
 
-    private static <T> void wakeQueue(Queue<Future.Waker<T>> queue, T result) {
-        while (!queue.isEmpty()) {
-            Future.Waker<T> waker = queue.remove();
-            waker.wake(result);
+    private static abstract class CommandCallback extends BluetoothGattCallback {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            throw new UnexpectedCallbackException();
         }
-    }
 
-    private static <T> void wakeQueueWithThrowable(Queue<Future.Waker<T>> queue, Throwable result) {
-        while (!queue.isEmpty()) {
-            Future.Waker<T> waker = queue.remove();
-            waker.wakeWithThrowable(result);
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            throw new UnexpectedCallbackException();
         }
-    }
 
-    private interface AsyncWithWaker<T> {
-        void withWaker(Future.Waker<T> waker);
-    }
-
-    private static <T> Future<T> asyncWithWaker(AsyncWithWaker<T> callback) {
-        Future.Waker<T> waker = Future.create();
-        try {
-            callback.withWaker(waker);
-        } catch (Throwable ex) {
-            waker.wakeWithThrowable(ex);
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            throw new UnexpectedCallbackException();
         }
-        return waker.getFuture();
     }
 }
